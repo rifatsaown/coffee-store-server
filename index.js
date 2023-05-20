79e664e3db15acd7db7437b9e4f57ca8f6f0ce43
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-21 01:53:38 +0600

    Mongo database Conected

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged Coffee deployment. You successfully connected to MongoDB!");
     } finally { }
 }
 run().catch(console.dir);
